542483248ad0b048d99ac6ab260df28ac6169d8a
Raf Gemmail <raf@dreamthought.com>
2018-07-07 17:32:17 +0100

    Marked some tests with ignores

--- src/test/java/com/example/parentcontrolservice/controller/ParentalControlControllerTest.java
+++ src/test/java/com/example/parentcontrolservice/controller/ParentalControlControllerTest.java
@@ -1,18 +1,31 @@
 package com.example.parentcontrolservice.controller;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * Tests the ParentalControlController
  */
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class ParentalControlControllerTest {
 
+  @Autowired
+  MockMvc mockMvc;
+
   @Test
+  @Ignore("TODO - confirm is this is a requirement")
   public void itShouldProvideAListOfAllParentalControlRatings() {
   }
 
 
   @Test
+  @Ignore("TODO - implement")
   public void itShouldValidateIfAMovieIsViewable() {
   }
 
