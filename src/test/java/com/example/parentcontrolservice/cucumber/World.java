2f2ad525da639bf7da5e62a3ee6d530650f4beda
Raf Gemmail <raf@dreamthought.com>
2018-07-07 16:29:52 +0100

    Failing ITs

--- src/test/java/com/example/parentcontrolservice/cucumber/World.java
+++ src/test/java/com/example/parentcontrolservice/cucumber/World.java
@@ -2,12 +2,16 @@ package com.example.parentcontrolservice.cucumber;
 
 import com.example.parentcontrolservice.cucumber.model.TestableParentalControlLevel;
 import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.ResultActions;
 
 /**
  * Created by raf on 6/07/18.
  */
 @Data
+@Component
 public class World {
   TestableParentalControlLevel parentalLevelPreference;
   TestableParentalControlLevel movieParentalLevel;
+  ResultActions response;
 }
--- src/test/java/com/example/parentcontrolservice/cucumber/steps/ParentalControllerSteps.java
+++ src/test/java/com/example/parentcontrolservice/cucumber/steps/ParentalControllerSteps.java
@@ -1,19 +1,37 @@
 package com.example.parentcontrolservice.cucumber.steps;
 
+import com.example.parentcontrolservice.ParentalControlServiceAT;
 import com.example.parentcontrolservice.cucumber.World;
 import com.example.parentcontrolservice.cucumber.model.TestableParentalControlLevel;
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 /**
  * Created by raf on 6/07/18.
  */
-public class ParentalControllerSteps implements En{
+@WebMvcTest(ParentalControlServiceAT.class)
+@ContextConfiguration(loader = SpringBootContextLoader.class, classes = ParentalControlServiceAT.class)
+public final class ParentalControllerSteps implements En{
 
-  public ParentalControllerSteps (World world) {
+  // TODO encapsulate in world
+  @Autowired
+  MockMvc mockMvc;
+
+  // TODO is not RESTful - change to either RPC style or provide filters of movie and preference
+  private static final String SERVICE_REQUEST_TEMPLATE = "/parentalcontrol/movie/%s/preference/%s";
+
+  public ParentalControllerSteps (@Autowired World world) {
     Given("mother has a parental control preference setting of (.+)", (String preferredLevel) -> {
       TestableParentalControlLevel preferenceLevel = TestableParentalControlLevel.lookupByRating.get( preferredLevel );
-
       //TODO handle NPE
       world.setParentalLevelPreference(preferenceLevel);
     });
@@ -24,14 +42,15 @@ public class ParentalControllerSteps implements En{
       world.setMovieParentalLevel(movieLevel);
     });
 
-    When("mother attempts to watch Jaws", () -> {
-      // Write code here that turns the phrase above into concrete actions
-      throw new PendingException();
+    When("mother attempts to watch (\\w+)", (String movie) -> {
+      TestableParentalControlLevel preferenceLevel = world.getParentalLevelPreference();
+      String url = String.format(SERVICE_REQUEST_TEMPLATE, movie, preferenceLevel.getParentalControlLevel());
+      ResultActions response = mockMvc.perform(get(url)).andExpect(status().isOk());
+      //world.setResponse(response);
     });
 
     Then("she will not be permitted to watch it", () -> {
-      // Write code here that turns the phrase above into concrete actions
-      throw new PendingException();
+          //world.getResponse().andExpect(jsonPath("$.viewable", is(false)));
     });
   };
 }
